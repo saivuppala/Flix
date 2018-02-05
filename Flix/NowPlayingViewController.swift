bc1cff92cec668ba7d4cedc8b63a2c78a0244363
saivuppala <svuppala@ucsd.edu>
2018-02-05 13:32:17 -0800

    done

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+         refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         
-        
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
+        tableView.rowHeight = 200
+        fetchMovies()
         
         
+        /*let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when the network request returns
+            if let error = error{
+                print(error.localizedDescription)
+            } else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                /*for movie in movies{
+                    let title = movie["title"] as! String
+                    print(title)
+                }*/
+            }
+        }
+        task.resume()*/
+        
+    }
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
+    }
+    func fetchMovies(){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -30,20 +64,34 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 print(dataDictionary)
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
+                self.tableView.reloadData()
+                
+                self.refreshControl.endRefreshing()
+                
+                /*for movie in movies{
+                 let title = movie["title"] as! String
+                 print(title)
+                 }*/
             }
         }
         task.resume()
-        
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string:baseURLString + posterPathString )!
+        cell.posterImageView.af_setImage(withURL: posterURL)
         return cell
     }
     
--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 
-# Project 1 - *Name of App Here*
+# Project 1 - *Flix*
 
-**Name of your app** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+**Flix** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
-Time spent: **X** hours spent in total
+Time spent: **5** hours spent in total
 
 ## User Stories
 
 The following **required** user stories are complete:
 
-- [ ] User sees app icon in home screen and styled launch screen (+1pt)
-- [ ] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
-- [ ] User can "Pull to refresh" the movie list (+2pt)
+- [X ] User sees app icon in home screen and styled launch screen (+1pt)
+- [X] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
+- [X] User can "Pull to refresh" the movie list (+2pt)
 - [ ] User sees a loading state while waiting for the movies to load (+2pt)
 
 The following **optional** user stories are implemented:
@@ -29,14 +29,14 @@ The following **additional** user stories are implemented:
 
 Please list two areas of the assignment you'd like to **discuss further with your peers** during the next class (examples include better ways to implement something, how to extend your app in certain ways, etc):
 
-1.
-2.
+1. Give a border around each movie.
+2. Be able to click each movie in order to get more details about it.
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://media.giphy.com/media/xThtap2Fz9Et3NKn4I/giphy.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
@@ -46,7 +46,7 @@ Describe any challenges encountered while building the app.
 
 ## License
 
-Copyright [yyyy] [name of copyright owner]
+Copyright [2018] [Sai Vuppala]
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
