03fbc1a87b3eaffa0af5c73f2561f55a0528e0d0
saivuppala <svuppala@ucsd.edu>
2018-02-05 10:41:38 -0800

    f

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -19,7 +19,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         tableView.dataSource = self
         
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/{movie_id}?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
