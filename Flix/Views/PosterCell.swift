e1c0187a68ea7d61d1305c36fdda95fa310ad6fe
saivuppala <svuppala@ucsd.edu>
2018-02-06 23:18:27 -0800

    fllix

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Sai Vuppala on 2/6/18.
+//  Copyright © 2018 Sai Vuppala. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
